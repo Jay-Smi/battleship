36879d6797d76cb9a2376ff9f5d4b294ee1c6c14
Jayson <38263580+gluttz@users.noreply.github.com>
2023-03-06 21:01:25 -0500

    move index.js again

--- index.js
+++
@@ -1,7 +0,0 @@
-import Game from "./src/scripts/components/Game.js";
-import ViewModel from "./src/scripts/ViewModel";
-import App from "./src/scripts/DOM/App.js";
-
-const model = new Game();
-const vm = new ViewModel(model);
-new App(vm, document.querySelector("#container"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./index.js",
+    entry: "./src/index.js",
     devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
